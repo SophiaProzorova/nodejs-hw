6f6dbf4ba2477297d818c9148d69f11622861e80
SophiaProzorova <sophia.prozorova@gmail.com>
2025-10-14 16:12:14 +0300

    small fixes

--- src/server.js
+++ src/server.js
@@ -6,21 +6,21 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// app.use(express.json());
-// app.use(cors());
-// app.use(pino({
-//     level: 'info',
-//     transport: {
-//         target: 'pino-pretty',
-//         options: {
-//             colorize: true,
-//             translateTime: 'HH:MM:ss',
-//             ignore: 'pid, hostname',
-//             messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-//             hideObject: true
-//         }
-//     }
-// }));
+app.use(express.json());
+app.use(cors());
+app.use(pino({
+    level: 'info',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            translateTime: 'HH:MM:ss',
+            ignore: 'pid, hostname',
+            messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+            hideObject: true
+        }
+    }
+}));
 
 app.use((req, res, next) => {
   console.log(`Time: ${new Date().toLocaleString()}`);
